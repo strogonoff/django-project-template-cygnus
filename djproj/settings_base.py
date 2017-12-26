97c7428160e303a5f8e9b9db91aeb5c8bdcc01d9
Anton Strogonoff <anton@strogonoff.name>
2017-12-26 15:43:09 +0900

    settings: paths to global templates & static assets

--- djproj/settings_base.py
+++ djproj/settings_base.py
@@ -42,7 +42,7 @@ ROOT_URLCONF = 'djproj.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -102,7 +102,7 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.11/howto/static-files/
-
-STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    ('static', os.path.join(BASE_DIR, 'static')),
+    ('node_modules', os.path.join(BASE_DIR, 'node_modules')),
+]
